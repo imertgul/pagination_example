17af2914cd42a262fb049edfebbff7ac51031c68
İbrahim Mert Gül <ibrahimmertgul@gmail.com>
2022-08-19 17:24:23 +0300

    fix movie result and add navigation to detail page

--- lib/view/widgets/movie_result_widget.dart
+++ lib/view/widgets/movie_result_widget.dart
@@ -1,4 +1,5 @@
 import 'package:dh_case/model/search_response.dart';
+import 'package:dh_case/view/movie_detail_page.dart';
 import 'package:flutter/material.dart';
 
 class MovieResultWidget extends StatelessWidget {
@@ -9,51 +10,59 @@ class MovieResultWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        // height: 700,
-        width: 250,
-        child: Column(
-          children: [
-            Expanded(
-                child: Stack(
-              children: [
-                Container(
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => MovieDetailPage(movieResult: movie)));
+        },
+        child: SizedBox(
+          // height: 700,
+          width: 250,
+          child: Column(
+            children: [
+              Expanded(
+                  child: Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white, border: Border.all()),
+                    child: movie.posterPath != null
+                        ? Image.network(
+                            fit: BoxFit.fitWidth,
+                            'https://image.tmdb.org/t/p/w500/${movie.posterPath}')
+                        : const Center(child: Text('Resim Bulunamadi')),
+                  ),
+                  if (movie.voteAverage != null)
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white, border: Border.all()),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Text(movie.voteAverage.toString()),
+                          ),
+                        ),
+                      ),
+                    )
+                ],
+              )),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
                   width: double.infinity,
                   decoration:
                       BoxDecoration(color: Colors.white, border: Border.all()),
-                  child: movie.posterPath != null
-                      ? Image.network(
-                          fit: BoxFit.fitWidth,
-                          'https://image.tmdb.org/t/p/w500/${movie.posterPath}')
-                      : const Center(child: Text('Resim Bulunamadi')),
+                  child: Text(movie.title),
                 ),
-                if (movie.voteAverage != null)
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white, border: Border.all()),
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Text(movie.voteAverage.toString()),
-                        ),
-                      ),
-                    ),
-                  )
-              ],
-            )),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                width: double.infinity,
-                decoration:
-                    BoxDecoration(color: Colors.white, border: Border.all()),
-                child: Text(movie.title),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
